bac551d5ab2bce407067a50e24af038b32080fb7
Vaibhav Raj <whybebhav@gmail.com>
2025-06-05 22:46:34 +0530

    logout fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -89,8 +89,14 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
     try {
-        res.cookie("jwt","",{maxAge:0});
-        res.status(200).json({message:"Logged out successfully"});
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            path: '/'
+        });
+
+        res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
         console.error("Error in logout controller",error.message);
         res.status(500).json({message:"Internal Server Error"});
